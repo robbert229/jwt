441b0fe4907b24777f25e8cc6389a8fd3f32f0da
Tyler Cook <tcook@ucbbank.com>
2018-11-08 11:13:07 -0600

    Fix typos in comments

--- jwt.go
+++ jwt.go
@@ -1,7 +1,7 @@
 // Package jwt is a barebones JWT implementation that supports just the bare necessities.
 package jwt
 
-// Header containins important information for encrypting / decryting
+// Header contains important information for encrypting / decrypting
 type Header struct {
 	Typ string `json:"typ"` // Token Type
 	Alg string `json:"alg"` // Message Authentication Code Algorithm - The issuer can freely set an algorithm to verify the signature on the token. However, some asymmetrical algorithms pose security concerns
